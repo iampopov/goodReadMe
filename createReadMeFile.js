b8bd253ccbd682db6ecb33a75e5831de95c34c61
Ivan Popov <ivantsar@gmail.com>
2020-05-02 21:45:09 -0600

    added more to the mark up

--- createReadMeFile.js
+++ createReadMeFile.js
@@ -1,9 +1,51 @@
+
+
 function createReadMeFile(data) {
-    return `
-  # ${data.GH}
-  ${data.fName}
-  
-  `;
+    return `## Description
+
+    ${data.description}
+    
+    ## Table of Contents 
+    
+    * [Installation](#installation)
+    
+    * [Usage](#usage)
+    
+    * [License](#license)
+    
+    * [Contributing](#contributing)
+    
+    * [Tests](#tests)
+    
+    * [Questions](#questions)
+    
+    ## Installation
+    
+    To install necessary dependencies, run the following command:
+    
+    \`\`\`
+    ${data.installation}
+    \`\`\`
+    
+    ## Usage
+    
+    ${data.usage}
+        
+    ## Contributing
+    
+    ${data.contributing}
+    
+    ## Tests
+    
+    To run tests, run the following command:
+    
+    \`\`\`
+    ${data.test}
+    \`\`\`
+    
+    ## Questions
+    
+    If you have any questions about the repo, open an issue or contact [${data.GH}](${data.url}) directly at ${data.email}.`;
   }
   
   module.exports = createReadMeFile;
--- index.js
+++ index.js
@@ -32,40 +32,41 @@ const input = [
     //     name: "title",
     //     message: "Please type your project's name?"
     //   },
-    //   {
-    //     type: "input",
-    //     name: "description",
-    //     message: "Please write a short description of your project"
-    //   },
+       {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project",
+        default: 'this is an app I developed so I can automate read me file development'
+      },
     //   {
     //     type: "list",
     //     name: "license",
     //     message: "What kind of license should your project have?",
     //     choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
     //   },
-    //   {
-    //     type: "input",
-    //     name: "installation",
-    //     message: "What command should be run to install dependencies?",
-    //     default: "npm i"
-    //   },
-    //   {
-    //     type: "input",
-    //     name: "test",
-    //     message: "What command should be run to run tests?",
-    //     default: "npm test"
-    //   },
-    //   {
-    //     type: "input",
-    //     name: "usage",
-    //     message: "What does the user need to know about using the repo?",
-    //     default: "It is an open project and everyone can contribute - please send and email requesting to be added as a contributor!"
-    //   },
-    //   {
-    //     type: "input",
-    //     name: "contributing",
-    //     message: "What does the user need to know about contributing to the repo?",
-    //   },
+      {
+        type: "input",
+        name: "installation",
+        message: "What command should be run to install dependencies?",
+        default: "npm i"
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: "npm test"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?",
+        default: "It is an open project and everyone can contribute - please send and email requesting to be added as a contributor!"
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "What does the user need to know about contributing to the repo?",
+      },
 
 ]
 
