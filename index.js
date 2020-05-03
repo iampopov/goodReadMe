f9ec128609935252b1a2de093f3ba9b40e71b119
Ivan Popov <ivantsar@gmail.com>
2020-05-02 20:56:56 -0600

    added basic questions

--- index.js
+++ index.js
@@ -2,3 +2,61 @@ const fs = require('fs');
 const filePath = require('path');
 const inquirer = require('inquirer');
 const createReadMeFile = require('./createReadMeFile');
+
+const input = [
+    {
+        type: "input",
+        name: "GH",
+        message: "Please type your GitHub username?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please type your email?"
+      },
+      {
+        type: "input",
+        name: "URL",
+        message: "the URL to your project?"
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "Please type your project's name?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project"
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "What command should be run to install dependencies?",
+        default: "npm i"
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: "npm test"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?",
+        default: "It is an open project and everyone can contribute - please send and email requesting to be added as a contributor!"
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "What does the user need to know about contributing to the repo?",
+      },
+      
+]
